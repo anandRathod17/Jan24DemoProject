f68573284dba7da79454416855cd7e61c5b98e6c
Anand Rathod <rathod.a86@gmail.com>
2024-04-06 11:30:08 +0530

    Branch 2 changes

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -1,9 +1,9 @@
 package p2;
 
 public class Test2 {
-
 	public static void main(String[] args) {
 		System.out.println("You are in Test2 file");
+		System.out.println("Branch B - You are in Test2 file line change 1");
+		System.out.println("Branch B - You are in Test2 file line change 2");
 	}
-
 }
