e18bcf8c5e73a54aab53e6d0da16d0f65bcbc3d7
Anand Rathod <rathod.a86@gmail.com>
2024-04-06 11:22:16 +0530

    Add changes from branch A

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -1,9 +1,8 @@
 package p2;
 
 public class Test2 {
-
 	public static void main(String[] args) {
 		System.out.println("You are in Test2 file");
+		System.out.println("Branch A - You are in Test2 file line number 6 changes");
 	}
-
 }
